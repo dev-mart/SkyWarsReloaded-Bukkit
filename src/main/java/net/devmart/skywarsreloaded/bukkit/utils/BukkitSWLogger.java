ae4378d22c6d313a596537dcb3b7cf3f2ead2968
lukasvdgaag <lukasvdgaag@gmail.com>
2023-12-16 13:06:15 +0100

    Renamed formatting method for logging.

--- src/main/java/net/devmart/skywarsreloaded/bukkit/utils/BukkitSWLogger.java
+++ src/main/java/net/devmart/skywarsreloaded/bukkit/utils/BukkitSWLogger.java
@@ -7,30 +7,30 @@ import java.util.logging.Logger;
 
 public class BukkitSWLogger extends AbstractSWLogger {
 
-    private final Logger bukkitLogger;
+    protected final Logger bukkitLogger;
 
     public BukkitSWLogger(SkyWarsReloaded pluginIn, Logger bukkitLoggerIn, boolean isDebugModeActive) {
         super(pluginIn, isDebugModeActive);
         this.bukkitLogger = bukkitLoggerIn;
     }
 
-    private String getParsedMessage(String message, Object... args) {
+    protected String formatMessage(String message, Object... args) {
         if (args == null || args.length == 0) return message;
         return String.format(message, args);
     }
 
     @Override
     public void info(String message, Object... args) {
-        this.bukkitLogger.info(getParsedMessage(message, args));
+        this.bukkitLogger.info(formatMessage(message, args));
     }
 
     @Override
     public void warn(String message, Object... args) {
-        this.bukkitLogger.warning(getParsedMessage(message, args));
+        this.bukkitLogger.warning(formatMessage(message, args));
     }
 
     @Override
     public void error(String message, Object... args) {
-        this.bukkitLogger.severe(getParsedMessage(message, args));
+        this.bukkitLogger.severe(formatMessage(message, args));
     }
 }

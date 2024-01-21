9e69462d5e935b26910f51f4ebd0a5f774181594
lukasvdgaag <lukasvdgaag@gmail.com>
2024-01-21 14:53:53 +0100

    Fixed auto-assigning teams with waiting lobby.

--- src/main/java/net/devmart/skywarsreloaded/bukkit/managers/BukkitGameInstanceManager.java
+++ src/main/java/net/devmart/skywarsreloaded/bukkit/managers/BukkitGameInstanceManager.java
@@ -1,13 +1,11 @@
 package net.devmart.skywarsreloaded.bukkit.managers;
 
-import net.devmart.skywarsreloaded.api.SkyWarsReloaded;
 import net.devmart.skywarsreloaded.api.game.GameTemplate;
 import net.devmart.skywarsreloaded.api.game.gameinstance.LocalGameInstance;
 import net.devmart.skywarsreloaded.bukkit.BukkitSkyWarsReloaded;
 import net.devmart.skywarsreloaded.bukkit.game.BukkitLocalGameInstance;
 import net.devmart.skywarsreloaded.core.manager.game.instance.CoreLocalGameInstanceManager;
 
-import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -27,29 +25,8 @@ public class BukkitGameInstanceManager extends CoreLocalGameInstanceManager {
         }
 
         LocalGameInstance localGameInstance = new BukkitLocalGameInstance((BukkitSkyWarsReloaded) skywars, UUID.randomUUID(), data);
-        WeakReference<LocalGameInstance> weakRef = new WeakReference<>(localGameInstance);
 
         this.registerGameInstance(localGameInstance);
-
-        if (skywars.getLogger().isDebugModeActive()) {
-            skywars.getLogger().debug("Debug mode active! Checking for game instance references...");
-            startCheckingForRefs(skywars, weakRef, "LocalGameInstance::" + localGameInstance.getWorldName());
-        } else {
-            weakRef = null;
-        }
         return CompletableFuture.completedFuture(localGameInstance);
-    }
-
-      private static void startCheckingForRefs(SkyWarsReloaded skywars, WeakReference weakRef, String name) {
-        if (weakRef == null) return;
-
-        skywars.getScheduler().runSyncTimer(() -> {
-            Object obj = weakRef.get();
-            if (obj == null) {
-                skywars.getLogger().debug("Object \"%s\" has no references!", name);
-            } else {
-                skywars.getLogger().debug("Object \"%s\" still has references!", name);
-            }
-        }, 0, 20);
     }
 }
